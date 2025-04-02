5a2f842172d1544039caccf54ef279e56845333e
Briyanssss <briyannicho14@gmail.com>
2025-04-02 08:34:23 +0700

    Initial commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/produk', 'ProdukController::index');
+$routes->get('/keranjang', 'TransaksiController::index');
