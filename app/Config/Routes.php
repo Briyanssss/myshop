3598ad4ca45bfb94f22fb79342e35117c0f24945
Briyanssss <briyannicho14@gmail.com>
2025-05-24 13:56:49 +0700

    Create, Update & Delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,17 @@ $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+$routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+$routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 $routes->get('profile', 'ProfileController::index', ['filter' => 'auth']);
